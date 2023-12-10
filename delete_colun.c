d8b540599c0217449478d3ad8f7de3f9a53620b0
Ndryl <113612316+Ndryl@users.noreply.github.com>
2023-12-10 16:34:30 -0300

    Update delete_colun.c

--- delete_colun.c
+++ delete_colun.c
@@ -1,53 +1,62 @@
 #include "updateTableList.h"
 
-
-void remove_coluna(char* nome) {
-  printf("Insira a coluna que você quer excluir: ");
-  int column_number;
-  scanf(" %d", &column_number);
-  FILE *arquivo;
-  char nome_arquivo[] = "../tables/tableList.txt";
-  arquivo = fopen(nome_arquivo, "r");
-  char nome_arquivoTemp[] = "../tables/arquivoTemp.txt";
-   FILE *arquivoTemp = fopen(nome_arquivoTemp, "w");
-
-  if(arquivo == NULL){
-      printf("Erro ao abrir o arquivo tableList.txt\n");
-      return;
-  }
-  if (arquivoTemp == NULL) {
-      perror("Erro ao criar o arquivo temporário\n");
-      fclose(arquivo);
-      return;
-  }
-
-  char linha[1000];
-
-  while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-    char *linha_copia = strdup(linha); // Faz uma cópia da linha para manipulação
-
-    // Tokeniza a linha usando '|' como delimitador
-    char *token = strtok(linha_copia, "|");
-    int coluna_atual = 1;
-    while (token != NULL) {
-      if (coluna_atual != column_number) {
-      // Escreve no arquivo temporário apenas se não for a coluna especificada
-        fprintf(arquivoTemp, "%s|", token);
-      }
-      token = strtok(NULL, "|");
-      coluna_atual++;
-    }
-    fprintf(arquivoTemp, "\n");
-
-    free(linha_copia); // Libera a memória alocada para a cópia da linha
+void remove_coluna(char *nome) {
+    FILE *file = fopen(nome, "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo %s\n", nome);
+        return;
     }
 
-  fclose(arquivo);
-  fclose(arquivoTemp);
+    printf("Insira o número da coluna que você deseja excluir: ");
+    int column_number;
+    if (scanf("%d", &column_number) != 1 || column_number <= 0) {
+        printf("Entrada inválida para o número da coluna.\n");
+        fclose(file);
+        return;
+    }
 
-  remove(nome_arquivo);
+    FILE *temp_file = fopen("temp.txt", "w");
+    if (temp_file == NULL) {
+        printf("Erro ao criar o arquivo temporário\n");
+        fclose(file);
+        return;
+    }
 
-  if (rename(nome_arquivoTemp, nome_arquivo) != 0) {
-    perror("Erro ao renomear o arquivo temporário");
-  }
+    char linha[1000];
+    while (fgets(linha, sizeof(linha), file) != NULL) {
+        size_t len = strlen(linha);
+        if (len > 0 && linha[len - 1] == '\n') {
+            linha[len - 1] = '\0'; // Remove o caractere de nova linha, se houver
+        }
+
+        char *token = strtok(linha, "|");
+        int coluna_atual = 1;
+        int primeiro_token = 1;
+
+        while (token != NULL) {
+            if (coluna_atual != column_number) {
+                if (!primeiro_token) {
+                    fprintf(temp_file, "|");
+                }
+                fprintf(temp_file, "%s", token);
+                primeiro_token = 0;
+            }
+            token = strtok(NULL, "|");
+            coluna_atual++;
+        }
+        fprintf(temp_file, "\n");
+    }
+
+    fclose(file);
+    fclose(temp_file);
+
+    // Remove o arquivo original e renomeia o temporário para o nome original
+    if (remove(nome) != 0) {
+        printf("Erro ao remover o arquivo original\n");
+        return;
+    }
+    if (rename("temp.txt", nome) != 0) {
+        printf("Erro ao renomear o arquivo temporário\n");
+        return;
+    }
 }
